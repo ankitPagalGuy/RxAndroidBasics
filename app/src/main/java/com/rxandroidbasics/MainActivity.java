0e2d68013124c6e973d1fd32ee61260d387c8852
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2016-10-12 21:39:52 +0530

    added a simple Hello World subscriber

--- app/src/main/java/com/rxandroidbasics/MainActivity.java
+++ app/src/main/java/com/rxandroidbasics/MainActivity.java
@@ -1,12 +1,26 @@
 package com.rxandroidbasics;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import rx.Observable;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
 
+
+  Observable.OnSubscribe observableAction = new Observable.OnSubscribe<String>() {
+    @Override
+    public void call(Subscriber<? super String> subscriber) {
+      subscriber.onNext("Hello, World!");
+      subscriber.onCompleted();
+    }
+  };
+
+  
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+
   }
 }
